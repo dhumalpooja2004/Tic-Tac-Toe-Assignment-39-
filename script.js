56439fd76bb86c88c203c443d11af77db2f486da
dhumalpooja2004 <dhumalpooja1204@gmail.com>
2024-12-24 12:19:15 +0530

    conditions added

--- script.js
+++ script.js
@@ -43,21 +43,42 @@ currentPlayerElement.innerText = currentPlayer;
 
 }
     function checkforWinner() {
-       if (board[0] == "X" && board[2] == "X") {
-        alert("Player X Wins");
-    }else if (board[3] == "X" && board[4] == "X" && board[5] == "X"){
-        alert("Player X Wins"); 
-    }else if (board[6] == "X" && board[7] == "X" && board[8] == "X"){
-        alert("Player X Wins"); 
-    }else if (board[0] == "X" && board[3] == "X" && board[6] == "X"){
-        alert("Player X Wins"); 
-    }else if (board[1] == "X" && board[4] == "X" && board[7] == "X"){
-        alert("Player X Wins"); 
-    }else if (board[2] == "X" && board[5] == "X" && board[8] == "X"){
-        alert("Player X Wins");
-    }else if (board[0] == "X" && board[4] == "X" && board[8] == "X"){
-        alert("Player X Wins"); 
-    }else if (board[2] == "X" && board[4] == "X" && board[6] == "X"){
-        alert("Player X Wins"); 
-    }
+       
+        if (board[0] == "X" && board[2] == "X") {
+            alert("Player X Wins");
+        }else if (board[3] == "X" && board[4] == "X" && board[5] == "X"){
+            alert("Player X Wins"); 
+        }else if (board[6] == "X" && board[7] == "X" && board[8] == "X"){
+            alert("Player X Wins"); 
+        }else if (board[0] == "X" && board[3] == "X" && board[6] == "X"){
+            alert("Player X Wins"); 
+        }else if (board[1] == "X" && board[4] == "X" && board[7] == "X"){
+            alert("Player X Wins"); 
+        }else if (board[2] == "X" && board[5] == "X" && board[8] == "X"){
+            alert("Player X Wins");
+        }else if (board[0] == "X" && board[4] == "X" && board[8] == "X"){
+            alert("Player X Wins"); 
+        }else if (board[2] == "X" && board[4] == "X" && board[6] == "X"){
+            alert("Player X Wins"); 
+        }
+
+
+
+        if (board[0] == "O" && board[2] == "O") {
+            alert("Player O Wins");
+        }else if (board[3] == "O" && board[4] == "O" && board[5] == "O"){
+            alert("Player O Wins"); 
+        }else if (board[6] == "O" && board[7] == "O" && board[8] == "O"){
+            alert("Player O Wins"); 
+        }else if (board[0] == "O" && board[3] == "O" && board[6] == "O"){
+            alert("Player O Wins"); 
+        }else if (board[1] == "O" && board[4] == "O" && board[7] == "O"){
+            alert("Player O Wins"); 
+        }else if (board[2] == "O" && board[5] == "O" && board[8] == "O"){
+            alert("Player O Wins");
+        }else if (board[0] == "O" && board[4] == "O" && board[8] == "O"){
+            alert("Player O Wins"); 
+        }else if (board[2] == "O" && board[4] == "O" && board[6] == "O"){
+            alert("Player O Wins"); 
+        }
     }
