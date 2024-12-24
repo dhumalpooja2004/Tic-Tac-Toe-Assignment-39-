0eff89a7b4d4f2cff5a5ba5e2d32180321c3cdc5
dhumalpooja2004 <dhumalpooja1204@gmail.com>
2024-12-24 11:28:39 +0530

    scrptJs file cretr

---
+++ script.js
@@ -0,0 +1,33 @@
+const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+let currentPlayer = "X";
+function renderBoard(){
+    const allBoxes = document.querySelectorAll(".box");
+    allBoxes.forEach((boxElement, i) => {
+      if (board[i] === "O") {
+        boxElement.innerHTML = '<img  src="player1.png" height="70px" alt="X" />';
+      } else if (board[i] === "X") {
+        boxElement.innerHTML = '<img src="player2.png"  height="70px" alt="O" />';
+      } else {
+        boxElement.innerHTML = "";
+      }
+    });
+  };
+  renderBoard();
+
+  function selectBox(boxNumber){
+    if(board[boxNumber] !== -1){
+      alert("Invalid Move")
+      return;
+    }
+    board[boxNumber] =currentPlayer;
+    renderBoard();
+     
+    if(currentPlayer === "X"){
+      currentPlayer = "O";
+    } else {
+      currentPlayer = "X";
+    }
+  }
+
+  const currentPlayerElement = document.getElementById("current-player");
+  currentPlayerElement.innerText =  currentPlayer;
